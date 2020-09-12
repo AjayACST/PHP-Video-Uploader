90dc32efb3479d897ed2cbb9987b5f112a67eb60
AjayACST <ajay@acst.co.nz>
2020-09-12 20:50:14 +1200

    Testing

--- meta.php
+++ meta.php
@@ -5,5 +5,5 @@ function meta($fileName,$thumb_name)
     <meta charset="utf-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
      <meta name="description" content="<?php echo $test ?>">
-     <meta name="og:image" content="<?php echo $thumb_name ?>"><?php
+     <meta name="og:video" content="<?php echo $thumb_name ?>"><?php
  }?>
