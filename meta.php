c716aae61898193fd9c32ed2ddf0e90f6870e06e
AjayACST <ajay@acst.co.nz>
2020-09-12 19:51:07 +1200

    Now fixed it

--- meta.php
+++ meta.php
@@ -1,9 +1,9 @@
 <?php
 
-function meta($fileDir,$thumb_name)
+function meta($fileName,$thumb_name)
 {?>
     <meta charset="utf-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
      <meta name="description" content="<?php echo $fileName ?>">
-     <meta name="description" content="<?php echo $thumb_name ?>"><?php
+     <meta name="og:image" content="<?php echo $thumb_name ?>"><?php
  }?>
